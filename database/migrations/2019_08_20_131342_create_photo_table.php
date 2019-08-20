025eb8cb7802a29d9a23645c84a514f5099b870c
webtoor <fityanalimunshi@gmail.com>
2019-08-20 20:14:54 +0700

    create migrations photo

---
+++ database/migrations/2019_08_20_131342_create_photo_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('laporan_id')->unsigned();
+            $table->string('photo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo');
+    }
+}
