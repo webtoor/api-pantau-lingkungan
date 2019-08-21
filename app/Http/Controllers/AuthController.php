2d61a19584d7d7c59bd6702aee720c52f9968c2c
webtoor <fityanalimunshi@gmail.com>
2019-08-21 12:37:51 +0700

    function register in authController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,8 +15,37 @@ class AuthController extends Controller
             'email'    => 'required|email|unique:users',
             'phonenumber' => 'required|numeric|min:10',
             'password' => 'required|string|min:5|confirmed',
-            'desa_kelurahan' => 'required'
+            'alamat' => 'required'
         ]);  
+
+       $results_user = User::create([
+            'firstname' => $request['firstname'],
+            'lastname' => $request['lastname'],
+            'email' => $request['email'],
+            'phonenumber' => $request['phonenumber'],
+            'gender' => $request['gender'],
+            'dob' => $request['dob'],
+            'alamat' => $request['alamat'],
+            'password' => $request['password']
+
+        ]);
+
+      $results_role = User_role::create([
+            'user_id' => $results_user->id,
+            'role_id' => '1'
+      ]);
+
+      if($resultUser && $resultRole){
+        return response()->json([
+            'status' => 'berhasil',
+            'message' => 'Berhasil membuat akun!'
+            ], 201);
+        }else{
+            return response()->json([
+            'status' => 'gagal',
+            'message' => 'Gagal membuat akun!'
+            ], 400);
+        }
 
     }
 }
