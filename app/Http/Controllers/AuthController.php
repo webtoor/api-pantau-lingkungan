f9355dced1120372a9ba577701f3c3bfc8349b73
webtoor <fityanalimunshi@gmail.com>
2019-08-21 09:29:21 +0700

    initiation register function + validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request){
+        $this->validate($request, [
+            'firstname' => 'required|string',
+            'lastname' => 'required|string',
+            'email'    => 'required|email|unique:users',
+            'phonenumber' => 'required|numeric|min:10',
+            'password' => 'required|string|min:5|confirmed',
+            'desa_kelurahan' => 'required'
+        ]);  
+
+    }
 }
