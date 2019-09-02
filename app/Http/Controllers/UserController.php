9c1870e9114df21b4ecca8991dba70e0772325ab
webtoor <fityanalimunshi@gmail.com>
2019-09-02 16:51:54 +0700

    install flysystem

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Laporan;
 use App\Location;
+use App\Photo;
 
 class UserController extends Controller
 {
     public function createLaporan(Request $request){
+        /* return $request->all(); */
          // Validate
+        /*  return response()->json([
+            'status' => '1',
+            'message' => base64_encode(file_get_contents($request->json('img')))
+            ]); */
+             $image = $request->json('img');
+             $image = str_replace('data:image/jpeg;base64,', '', $image);
+             $image = str_replace(' ', '+', $image);         
+             $imageName = 'image_'.time().str_random(10).'.png';
+             \Storage::disk('public')->put($imageName,base64_decode($image));     
+
+
          $this->validate($request, [
             'user_id' => 'required',
             'judul' => 'required|string',
@@ -40,7 +53,12 @@ class UserController extends Controller
                'accuracy' => $request->json('accuracy'),
            ]);
 
-           if($result_laporan && $result_location){
+           $result_photo = Photo::create([
+             'laporan_id' => $result_laporan->id,
+             'photo' => $imageName,
+           ]);
+
+           if($result_laporan && $result_location && $result_photo){
             return response()->json([
                 'status' => '1',
                 'message' => 'Sukses mengirim laporan!'
